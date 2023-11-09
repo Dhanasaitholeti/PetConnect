67dd146b3777c7c1ddfecbfe5defa4244f34565f
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-11-09 20:18:28 +0530

    added redux to the project and set the base

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./services/redux/store.ts";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Router>
+    <ChakraProvider>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
