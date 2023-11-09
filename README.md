e1cffb7ee13a4aa24067d8121bc3d552c980ce3b
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-11-09 20:42:27 +0530

    added docker support to enable containarization

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Mono Repo of a pet-E-commerce application
---
+++ bin/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ $1 = "prod" || $1 = "dev" ]] && [[ $2 = "down" || $2 = "up" ]]; then
+    cd ..
+    fileEnv="docker-compose.${1}.yaml"
+    downOrUp=$2
+    echo "Running docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp"
+    docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp
+else
+    echo "Need to follow format ./deploy.sh prod|dev down|up"
+fi
