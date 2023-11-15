5a914ebea27c6ea3b71ca8b7666d9a7eed71594c
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-11-15 08:53:41 +0530

    added filtering options on homescreen

--- client/src/components/pet/MainSection.tsx
+++ client/src/components/pet/MainSection.tsx
@@ -7,11 +7,32 @@ import { PetType } from "../../utils/types";
 import { updatePets } from "../../services/redux/slices/pet.slice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../services/redux/store";
+import { useLocation } from "react-router-dom";
 
 const MainSection = () => {
-  const dispatch = useDispatch();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const price = queryParams.get("price");
+  const category = queryParams.get("category");
 
+  const dispatch = useDispatch();
   const { error, pets } = useSelector((state: RootState) => state.PetReducer);
+
+  let filteredPets = pets;
+
+  if (category) {
+    filteredPets =
+      pets &&
+      pets?.filter((pet) => pet.category === category.toLocaleLowerCase());
+  }
+
+  if (price) {
+    if (price === "low-to-high") {
+      filteredPets?.sort((a, b) => a.price - b.price);
+    } else if (price === "high-to-low") {
+      filteredPets?.sort((a, b) => b.price - a.price);
+    }
+  }
 
   useQuery({
     queryKey: ["pets"],
@@ -34,7 +55,9 @@ const MainSection = () => {
         {error ? (
           <h1>error occuredd</h1>
         ) : pets ? (
-          pets.map((entry: PetType) => <PetCard Pet={entry} key={entry.id} />)
+          filteredPets?.map((entry: PetType) => (
+            <PetCard Pet={entry} key={entry.id} />
+          ))
         ) : (
           <h1>Loading.....</h1>
         )}
